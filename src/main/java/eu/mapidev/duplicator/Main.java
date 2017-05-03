b4b8c7e2d8a65c3536ba0f147aa0da0b4084c6c8
Mark <mpietryga93@gmail.com>
2017-05-04 01:29:14 +0200

    Add checking arguments

--- src/main/java/eu/mapidev/duplicator/Main.java
+++ src/main/java/eu/mapidev/duplicator/Main.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 public class Main {
 
     public static void main(String[] args) {
+
         Properties prop = new Properties();
 
         try {
@@ -15,9 +16,22 @@ public class Main {
                     .getCodeSource().getLocation().getPath());
             String configPath = mainPath.getParentFile().getAbsolutePath();
             prop.load(new FileInputStream(configPath + "/lp.properties"));
-            System.out.println(prop.getProperty("id"));
+            System.out.println("id=" + prop.getProperty("id"));
         } catch (IOException ex) {
             ex.printStackTrace();
+        }
+
+        if (args == null) {
+            throw new IllegalStateException("Arguments can not be null!");
+        } else if (args.length == 3) {
+            //todo
+        } else if (args.length == 4) {
+            //todo
+        } else {
+            System.out.println("You must specify at least 3 or 4 input arguments e.g. \n"
+                    + "\"Hello World!\" 1 5 or \n"
+                    + "\"Hello World!\" 1 2 10 \n"
+                    + "\"Hello $i \" 1 5");
         }
 
     }
