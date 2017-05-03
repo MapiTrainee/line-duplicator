a25c48e92519b48e94e685c8b19acee68264ce9f
Mark <mpietryga93@gmail.com>
2017-05-04 01:06:06 +0200

    Add properties loader

--- src/main/java/eu/mapidev/duplicator/Main.java
+++ src/main/java/eu/mapidev/duplicator/Main.java
@@ -1,8 +1,24 @@
 package eu.mapidev.duplicator;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
 
     public static void main(String[] args) {
+        Properties prop = new Properties();
+
+        try {
+            File mainPath = new File(Main.class.getProtectionDomain()
+                    .getCodeSource().getLocation().getPath());
+            String configPath = mainPath.getParentFile().getAbsolutePath();
+            prop.load(new FileInputStream(configPath + "/lp.properties"));
+            System.out.println(prop.getProperty("id"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
     }
 }
